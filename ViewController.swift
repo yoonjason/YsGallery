4fcf2d4fd0ee0badd4775fc75a51e0128d9894fa
Bradley.yoon <bradley.yoon@kakaocorp.com>
2021-12-16 14:05:33 +0900

    add: First Commit

---
+++ ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  YsGallery
+//
+//  Created by Bradley.yoon on 2021/12/16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
